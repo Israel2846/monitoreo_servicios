bdda9198bb2eb26813b5a23c54ed056a5b68e333
Israel Colín <israaacolin@gmail.com>
2023-11-09 16:03:10 -0600

    Agregando intervalo de 5 segundos

--- resources/js/estado_actual.js
+++ resources/js/estado_actual.js
@@ -2,25 +2,26 @@ const $grafica = document.querySelector("#grafica");
 const etiquetas = [1, 2, 3, 4]
 let datosTelmex;
 
-$.ajax({
-    type: 'GET',
-    url: 'estado',
-    success: function (datos) {
-        datosTelmex = {
-            label: "Telmex",
-            data: [datos[0], datos[1], datos[2], datos[3],],
-            backgroundColor: 'rgba(54, 162, 235, 0.1)',
-            borderColor: 'rgba(54, 162, 235, 1)',
-            borderWidth: 1,
-        };
+setInterval(() => {
+    $.ajax({
+        type: 'GET',
+        url: 'estado',
+        success: function (datos) {
+            datosTelmex = {
+                label: "Telmex",
+                data: [datos[0], datos[1], datos[2], datos[3]],
+                backgroundColor: 'rgba(54, 162, 235, 0.1)',
+                borderColor: 'rgba(54, 162, 235, 1)',
+                borderWidth: 1,
+            };
 
-        console.log(datosTelmex);
-        crearChart(etiquetas, datosTelmex);
-    },
-    error: function (error) {
-        console.log(error);
-    }
-})
+            crearChart(etiquetas, datosTelmex);
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    })
+}, 5000);
 
 function crearChart(etiquetas, datos) {
     new Chart($grafica, {
@@ -40,5 +41,3 @@ function crearChart(etiquetas, datos) {
         }
     });
 }
-
-
